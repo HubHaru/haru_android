e4a818b913cde2694ec321d0e7880974ce186aa8
zeropol2 <zeropol2@gmail.com>
2014-07-12 17:45:53 +0900

    [zeropol2] 메인 화면 1차 개발 데이터는 더미 테이터 스크롤 효과 구현

--- haru/src/com/projects/haru/HaruApp.java
+++ haru/src/com/projects/haru/HaruApp.java
@@ -22,16 +22,5 @@ public class HaruApp extends Application {
 	
 	private void init() {
 		DbApi.getInstance(this);
-		//초기데이터가 있어야 하니깐 일단 임시데이터 박아두자
-		HelloInfo info = new HelloInfo();
-		info.title = "타이틀1";
-		info.content = "내용1";
-		DbApi.getInstance().insertHello(info);
-		info.title = "타이틀2";
-		info.content = "내용2";
-		DbApi.getInstance().insertHello(info);
-		info.title = "타이틀3";
-		info.content = "내용3";
-		DbApi.getInstance().insertHello(info);
 	}
 }
--- haru/src/com/projects/haru/MainActivity.java
+++ haru/src/com/projects/haru/MainActivity.java
@@ -1,20 +1,29 @@
 package com.projects.haru;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Toast;
 
 import com.projects.haru.component.ActivityBase;
 import com.projects.haru.datamanager.AsyncDataLoader.OnDataLoadListener;
-import com.projects.haru.datamanager.HelloDataManager;
-import com.projects.haru.dto.HelloDto;
-import com.projects.haru.ui.HelloListView;
-import com.projects.haru.ui.HelloListView.UserActionListener;
+import com.projects.haru.datamanager.TaskDataManager;
+import com.projects.haru.dto.TaskDto;
+import com.projects.haru.ui.DayViewFooter;
+import com.projects.haru.ui.DayViewListView;
+import com.projects.haru.ui.DayViewListView.OnScrollDirectionListener;
+import com.projects.haru.ui.DayViewListView.UserActionListener;
+import com.projects.haru.ui.DayViewTopBar;
 
 public class MainActivity extends ActivityBase{
     
-	private HelloListView mListView;
+	private DayViewTopBar mTopBar;
+	private DayViewListView mListView;
+	private DayViewFooter mFooter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,12 +33,19 @@ public class MainActivity extends ActivityBase{
     @Override
     protected void initLayout() {
     	setContentView(R.layout.activity_main);
-    	mListView = (HelloListView)findViewById(R.id.activity_main_hello_listview);
+    	mListView = (DayViewListView)findViewById(R.id.activity_main_day_view_listview);
     	mListView.setUserActionLisetner(mUserActionListener);
+    	mTopBar = new DayViewTopBar(this);
+    	mTopBar.setData(Calendar.getInstance());
+    	mTopBar.setUserActionListener(mTopBarUserActionListener);
+    	mListView.addParallaxedHeaderView(mTopBar);
+    	mFooter = (DayViewFooter)findViewById(R.id.activity_main_day_view_footer);
+    	mFooter.setUserActionListener(mFooterUserActionListener);
+    	mListView.setOnScrollDirectionListener(mOnScrollDirectionListener);
     }
     
     private void loadData() {
-    	HelloDataManager.getInstance().loadHelloList(mOnDataLoadListener);
+    	TaskDataManager.getInstance().loadTaskList(mOnDataLoadListener, Calendar.getInstance());
     }
     
     private UserActionListener mUserActionListener = new UserActionListener() {
@@ -41,10 +57,10 @@ public class MainActivity extends ActivityBase{
 		}
 	};
 	
-	private OnDataLoadListener<ArrayList<HelloDto>> mOnDataLoadListener = new OnDataLoadListener<ArrayList<HelloDto>>() {
+	private OnDataLoadListener<ArrayList<TaskDto>> mOnDataLoadListener = new OnDataLoadListener<ArrayList<TaskDto>>() {
 		
 		@Override
-		public void onDataLoad(ArrayList<HelloDto> data) {
+		public void onDataLoad(ArrayList<TaskDto> data) {
 			mListView.addList(data);
 			if(null == mListView.getAdapter()) {
 				mListView.setAdapter();
@@ -56,6 +72,57 @@ public class MainActivity extends ActivityBase{
 		@Override
 		public void onDataLoadFail(Exception e) {
 			Toast.makeText(MainActivity.this, "데이터 로드 실패", Toast.LENGTH_SHORT).show();
+		}
+	};
+	
+	private DayViewTopBar.UserActionListener mTopBarUserActionListener = new DayViewTopBar.UserActionListener() {
+		
+		@Override
+		public void goWeekView(Calendar cal) {
+			Toast.makeText(MainActivity.this, "주별 보기로 이동할겁니다만,,크큭", Toast.LENGTH_SHORT).show();
+		}
+	};
+	
+	private DayViewFooter.UserActionListener mFooterUserActionListener = new DayViewFooter.UserActionListener() {
+		
+		@Override
+		public void goSetting() {
+			Toast.makeText(MainActivity.this, "설정 페이지로 이동할겁니다만,,크큭", Toast.LENGTH_SHORT).show();
+		}
+		
+		@Override
+		public void goAdd() {
+			Toast.makeText(MainActivity.this, "추가 페이지로 이동할겁니다만,,크큭", Toast.LENGTH_SHORT).show();
+		}
+	};
+	
+	private DayViewListView.OnScrollDirectionListener mOnScrollDirectionListener = new OnScrollDirectionListener() {
+		
+		@Override
+		public void onScrollUp() {
+			if(View.GONE == mFooter.getVisibility()) {
+				mFooter.setVisibility(View.VISIBLE);
+				Animation fadeInAnim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.fade_in);
+				mFooter.startAnimation(fadeInAnim);
+			}
+		}
+		
+		@Override
+		public void onScrollDown() {
+			if(View.VISIBLE == mFooter.getVisibility()) {
+				Animation fadeOutAnim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.fade_out);
+				mFooter.startAnimation(fadeOutAnim);
+				mFooter.setVisibility(View.GONE);
+			}
+		}
+		
+		@Override
+		public void onScrollStop() {
+			if(View.GONE == mFooter.getVisibility()) {
+				mFooter.setVisibility(View.VISIBLE);
+				Animation fadeInAnim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.fade_in);
+				mFooter.startAnimation(fadeInAnim);
+			}
 		}
 	};
 
